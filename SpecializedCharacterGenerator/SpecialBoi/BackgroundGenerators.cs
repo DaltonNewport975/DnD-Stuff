6b2a929ce5d1819b0927a7857936822abb13c556
Dalton Newport <dalton.e.newport-1@ou.edu>
2019-09-07 17:20:05 -0500

    Made the collections for reading in the files for character generation

---
+++ SpecializedCharacterGenerator/SpecialBoi/BackgroundGenerators.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SpecialBoi
+{
+    class BackgroundGenerators
+    {
+        private Dictionary<string, string> Feats = new Dictionary<string, string>();
+        private Dictionary<string, string> Races = new Dictionary<string, string>();
+        private Dictionary<string, string> Classes = new Dictionary<string, string>();
+        private Dictionary<string, string> Backgrounds = new Dictionary<string, string>();
+
+        public static void StartUp()
+        {
+            string[] feats = File.ReadAllLines("feats.txt");
+            string[] races = File.ReadAllLines("races.txt");
+            string[] classes = File.ReadAllLines("classes.txt");
+            string[] backgrounds = File.ReadAllLines("backgrounds.txt");
+        }
+    }
+}
