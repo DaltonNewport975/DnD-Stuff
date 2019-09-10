edfb036d9089a62002e891a528df53bf0512a1e2
Dalton Newport <dalton.e.newport-1@ou.edu>
2019-09-10 14:23:02 -0500

    Made the stat roller and started to do the min/maxing prioritization

--- SpecializedCharacterGenerator/SpecialBoi/BackendGenerators.cs
+++ SpecializedCharacterGenerator/SpecialBoi/BackendGenerators.cs
@@ -13,6 +13,7 @@ namespace SpecialBoi
         public static Dictionary<string, string> Races = new Dictionary<string, string>();
         public static Dictionary<string, string> Classes = new Dictionary<string, string>();
         public static Dictionary<string, string> Backgrounds = new Dictionary<string, string>();
+        public static Dictionary<string, string> Priorites = new Dictionary<string, string>();
 
         public static void StartUp()
         {
@@ -21,12 +22,14 @@ namespace SpecialBoi
             string[] races = File.ReadAllLines("races.txt");
             string[] classes = File.ReadAllLines("classes.txt");
             string[] backgrounds = File.ReadAllLines("backgrounds.txt");
+            string[] priorities = File.ReadAllLines("priorities.txt");
 
             //sort the arrays
             Array.Sort(feats);
             Array.Sort(races);
             Array.Sort(classes);
             Array.Sort(backgrounds);
+            Array.Sort(priorities);
 
             string tempKey;
             string tempEntry;
@@ -59,6 +62,13 @@ namespace SpecialBoi
                 tempEntry = i.Substring(i.IndexOf(",") + 1);
 
                 Backgrounds.Add(tempKey, tempEntry);
+            }
+            foreach (string i in priorities)
+            {
+                tempKey = i.Substring(0, i.IndexOf(","));
+                tempEntry = i.Substring(i.IndexOf(",") + 1);
+
+                Priorites.Add(tempKey, tempEntry);
             }
         }
 
@@ -94,6 +104,88 @@ namespace SpecialBoi
                     return "Subclass:";
             }
 
+        }
+
+        private static int[] characterRoller()
+        {
+
+            //variables
+            int[] roll1 = new int[4];
+            int[] roll2 = new int[4];
+            int[] roll3 = new int[4];
+            int[] roll4 = new int[4];
+            int[] roll5 = new int[4];
+            int[] roll6 = new int[4];
+            int i = 0;
+
+            int[] rolls = new int[6];
+
+            Random die = new Random();
+
+            //roll 4d6 6 times
+            for (i = 0; i < 4; i++)
+            {
+                roll1[i] = die.Next(1, 7);
+            }
+
+            for (i = 0; i < 4; i++)
+            {
+                roll2[i] = die.Next(1, 7);
+            }
+
+            for (i = 0; i < 4; i++)
+            {
+                roll2[i] = die.Next(1, 7);
+            }
+
+            for (i = 0; i < 4; i++)
+            {
+                roll3[i] = die.Next(1, 7);
+            }
+
+            for (i = 0; i < 4; i++)
+            {
+                roll4[i] = die.Next(1, 7);
+            }
+
+            for (i = 0; i < 4; i++)
+            {
+                roll5[i] = die.Next(1, 7);
+            }
+
+            for (i = 0; i < 4; i++)
+            {
+                roll6[i] = die.Next(1, 7);
+            }
+
+            //sort the unadjusted arrays
+            Array.Sort(roll1);
+            Array.Sort(roll2);
+            Array.Sort(roll3);
+            Array.Sort(roll4);
+            Array.Sort(roll5);
+            Array.Sort(roll6);
+
+            //sum the top 3 and add them to the arraylist
+            rolls[0] = roll1[1] + roll1[2] + roll1[3];
+            rolls[1] = roll2[1] + roll2[2] + roll2[3];
+            rolls[2] = roll3[1] + roll3[2] + roll3[3];
+            rolls[3] = roll4[1] + roll4[2] + roll4[3];
+            rolls[4] = roll5[1] + roll5[2] + roll5[3];
+            rolls[5] = roll6[1] + roll6[2] + roll6[3];
+
+            Array.Sort(rolls);
+
+            return rolls;
+
+        }
+
+        private static void setPriorities(string clas, string subclass)
+        {
+            switch (clas)
+            {
+                
+            }
         }
     }
 }
